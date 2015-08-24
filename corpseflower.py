61db6fee83c27c0319cd329db6caa883498d161e
hsw28 <holdbackaglacier@yahoo.com>
2015-08-24 12:54:45 -0400

    added more data for other plants, have to fix up gustavus

--- corpseflower.py
+++ corpseflower.py
@@ -4,12 +4,14 @@ import matplotlib.cbook as cbook
 import numpy as np
 from pylab import plotfile, show, gca
 
-data1 = np.genfromtxt('corpse.csv', delimiter=',', skip_header=1, names=['a','b','c','d'])
+data1 = np.genfromtxt('corpseflower.csv', delimiter=',', skip_header=1, names=['a','b','c','d', 'e', 'f'])
 
 fig = plt.figure()
 ax1 = fig.add_subplot(111)
-ax1.plot(data1['a'], data1['c'], color='r', linewidth=5, label='Spike')
-ax1.plot(data1['a'], data1['d'], color='b', linewidth=5, label='StinkyDGB')
+ax1.plot(data1['a'], data1['c'], color='r', linewidth=3, label='Spike')
+ax1.plot(data1['a'], data1['d'], color='b', linewidth=3, label='StinkyDGB')
+ax1.plot(data1['a'], data1['e'], color='g', linewidth=3, label='Huntington 2014')
+ax1.plot(data1['a'], data1['f'], color='indigo', linewidth=3, label='Gustavus 2007 (first bloom)')
 
 #plt.axis([0,13,0,100])
 
