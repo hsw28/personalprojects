f18d482f2ff25acc22cce78a4fb47819fa35cae1
hsw28 <holdbackaglacier@yahoo.com>
2015-08-24 19:06:40 -0400

    updated data and got rid of gustavus track on graph

--- corpseflower.py
+++ corpseflower.py
@@ -11,7 +11,7 @@ ax1 = fig.add_subplot(111)
 ax1.plot(data1['a'], data1['c'], color='r', linewidth=3, label='Spike')
 ax1.plot(data1['a'], data1['d'], color='b', linewidth=3, label='StinkyDGB')
 ax1.plot(data1['a'], data1['e'], color='g', linewidth=3, label='Huntington 2014')
-ax1.plot(data1['a'], data1['f'], color='indigo', linewidth=3, label='Gustavus 2007 (first bloom)')
+
 
 #plt.axis([0,13,0,100])
 
@@ -19,6 +19,6 @@ plt.legend(loc=2)
 plt.xlabel('Day')
 plt.ylabel('Height')
 
-plt.title('Comparison of Spike and StinkyDGB, ending with StinkyDGB Bloom')
+plt.title('Comparison of Spike and Other Corpse Flowers')
 
 show()
